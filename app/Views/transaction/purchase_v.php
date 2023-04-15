db49ea7f09127c6e6345371f52e95212f3391fa1
ibadi ichsan <ihsan.dulu@gmail.com>
2023-04-15 14:15:11 +0700

    require

--- app/Views/transaction/purchase_v.php
+++ app/Views/transaction/purchase_v.php
@@ -65,7 +65,7 @@
                                 <div class="form-group">
                                     <label class="control-label col-sm-2" for="purchase_date">Tgl Pembelian:</label>
                                     <div class="col-sm-10">
-                                        <input type="date" autofocus class="form-control" id="purchase_date" name="purchase_date" placeholder="" value="<?= $purchase_date; ?>">
+                                        <input required type="date" autofocus class="form-control" id="purchase_date" name="purchase_date" placeholder="" value="<?= $purchase_date; ?>">
                                     </div>
                                 </div>    
                                 <div class="form-group">
@@ -84,8 +84,8 @@
                                             ->get();
                                         //echo $this->db->getLastQuery();
                                         ?>
-                                        <select class="form-control select" id="supplier_id" name="supplier_id">
-                                            <option value="0" <?= ($supplier_id == "0") ? "selected" : ""; ?>>Pilih Supplier</option>
+                                        <select required class="form-control select" id="supplier_id" name="supplier_id">
+                                            <option value="" <?= ($supplier_id == "") ? "selected" : ""; ?>>Pilih Supplier</option>
                                             <?php
                                             foreach ($supplier->getResult() as $supplier) { ?>
                                                 <option value="<?= $supplier->supplier_id; ?>" <?= ($supplier_id == $supplier->supplier_id) ? "selected" : ""; ?>><?= $supplier->supplier_name; ?></option>
